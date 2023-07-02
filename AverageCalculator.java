808b055ebb5cf10bd10d548a69466c379989a0dc
pragatisj <pragatijadhav2121@gmail.com>
2023-07-02 14:36:18 +0530

    Practice Q 3

---
+++ AverageCalculator.java
@@ -0,0 +1,30 @@
+  import java.util.Scanner;
+
+
+  
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the count of numbers: ");
+        int count = scanner.nextInt();
+
+        int sum = 0;
+        int i = 0;
+        do {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            sum += number;
+            i++;
+        } while (i < count);
+
+        double average = (double) sum / count;
+        System.out.println("Average: " + average);
+
+        scanner.close();
+    }
+}
+
+    
+
